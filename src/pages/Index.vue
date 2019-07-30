37139b201f0ad6f3b0455428995f62e23011ce95
Thomas Cober <tcober5@gmail.com>
2019-07-29 23:25:46 -0500

    Adding repo link

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,7 +12,7 @@
     </div>
     <p class="home-links">
       <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <a href="https://github.com/tcober/starGridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
   </Layout>
 </template>
